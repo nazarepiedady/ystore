961e74598ec6f1794f8df74e953e930b79a341f2
Nazaré da Piedade <nazarepiedady@gmail.com>
2020-11-06 07:59:23 +0100

    Create a computed property to store the total of item in cart

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,5 +13,13 @@ const vm = new Vue({
       return value.toLocaleString(
         'en-US', { style: 'currency', currency: 'USA' });
     }
+  },
+  computed: {
+    cartTotal() {
+      let total = 0;
+      if (this.shoppingList.length)
+        this.shoppingList.forEach(item => { total += item.price; });
+      return total;
+    }
   }
 });
